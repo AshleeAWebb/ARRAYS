cef2db2acf7545c2ed0dfb81551a3da97c350cf5
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-01-11 17:40:26 -0500

    Add array calls

---
+++ arrays/arrays.js
@@ -0,0 +1,18 @@
+var dogs = ["Josie", "Doris", "Hammy", "Fay"];
+var dogAges = [3,9,3,8];
+var dogInfos = [true,true,false,true]
+
+dogs.pop();
+
+console.log(dogs);
+//Removes the last element from an array and returns that element.
+
+dogAges.slice();
+
+console.log(dogAges)
+//Extracts a section of the calling array and returns a new array.
+
+dogInfos.push();
+
+console.log(dogInfos)
+//Adds one or more elements to the end of an array, and returns the new length of the array.
